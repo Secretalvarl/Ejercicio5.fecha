8dbff3f868d590b3d4ea56959d4b08411e3b2fe6
DESKTOP-R8KP20E\Profesor <csi23-agarera@colegioaltair.net>
2024-02-06 14:09:06 +0100

    Agregar archivos de proyecto.

---
+++ Ejercicio5.fecha/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Ejercicio5.fecha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escriba la primera fecha con este formato dd-MM-yyyy: ");
+
+            if (DateTime.TryParse(Console.ReadLine(), out DateTime rimaeraFecha))
+            {
+                Console.WriteLine("Escriba la segunda fecha con este formato dd-MM-yyyy: ");
+
+                if (DateTime.TryParse(Console.ReadLine(), out DateTime fecha2))
+                {
+                    TimeSpan resta = fecha2 - rimaeraFecha;
+
+                    Console.WriteLine("Faltan: " + resta.TotalDays + "días, " + resta.TotalHours + "horas, " + resta.TotalMinutes + "minutos, " + resta.TotalSeconds + "segundos.");
+                } 
+            }
+        }
+    }
+}
